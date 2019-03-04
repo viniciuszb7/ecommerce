89e36b4b82ad402b2db5e51ca374835bcf7eaf29
VZB <batarra7@gmail.com>
2019-03-04 18:02:17 -0300

    Criando PageAdmin

--- index.php
+++ index.php
@@ -4,14 +4,23 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
 
-    $page = new Hcode\Page();
+    $page = new Page();
+
+    $page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+
+    $page = new PageAdmin();
 
     $page->setTpl("index");
 
